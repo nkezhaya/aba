19bb211e2ba9e0da5e930ac12aa51fea81e65a90
Nick Kezhaya <nick@whitepaperclip.com>
2018-08-17 09:47:25 -0500

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -2,28 +2,31 @@ defmodule ABA.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :aba,
-     version: "0.1.0",
-     elixir: "~> 1.5",
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :aba,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
-    [extra_applications: [:logger],
-     mod: {ABA, []}]
+    [extra_applications: [:logger], mod: {ABA, []}]
   end
 
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :doc}]
+    [{:ex_doc, ">= 0.0.0", only: :doc}, {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false}]
   end
 
   defp package do
-    [licenses: ["MIT"],
-     maintainers: ["Nick Kezhaya <nick@whitepaperclip.com>"],
-     links: %{"GitHub" => "https://github.com/whitepaperclip/ABA"}]
+    [
+      licenses: ["MIT"],
+      maintainers: ["Nick Kezhaya <nick@whitepaperclip.com>"],
+      links: %{"GitHub" => "https://github.com/whitepaperclip/ABA"}
+    ]
   end
 
   defp description do
